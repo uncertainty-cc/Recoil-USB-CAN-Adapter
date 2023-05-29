6f80b4c667e63f683e950c24cffb135ff76ee92a
-T.K.- <t_k_233@outlook.com>
2023-05-29 11:55:09 -0700

    FIX: fix CAN FIFO blocking issue, increase LED brightness

--- firmware/Recoil-USB-CAN-Adapter-G431/Core/Src/main.c
+++ firmware/Recoil-USB-CAN-Adapter-G431/Core/Src/main.c
@@ -269,7 +269,7 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pin = GPIO_PIN_6|GPIO_PIN_7;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
-  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
